e9a9d30334201fb4d85e104809337e3ecbc9950f
yulim08 <yulim020823@gmail.com>
2024-10-30 16:01:10 +0900

    Update app.py

--- app.py
+++ app.py
@@ -30,17 +30,7 @@ st.subheader("제출된 건의사항")
 try:
     suggestions_df = pd.read_csv(file_path, names=['Name', 'Email', 'Suggestion'])
     st.write(suggestions_df)
-
-    # 삭제할 건의사항 선택
-    suggestion_to_delete = st.selectbox("삭제할 건의사항 선택", suggestions_df['Suggestion'].tolist())
-
-    if st.button("삭제"):
-        # 선택한 건의사항 삭제
-        suggestions_df = suggestions_df[suggestions_df['Suggestion'] != suggestion_to_delete]
-        suggestions_df.to_csv(file_path, index=False, header=True)  # 업데이트된 데이터 저장
-        st.success("건의사항이 삭제되었습니다!")
-        st.experimental_rerun()  # 페이지 새로 고침
-
 except FileNotFoundError:
     st.write("아직 제출된 건의사항이 없습니다.")
+
 
